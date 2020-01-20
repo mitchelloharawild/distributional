fd675781568a633df2ce0cf08f6f133994555b71
Mitchell <mail@mitchelloharawild.com>
2020-01-20 17:37:50 +1100

    Added hilo tests

---
+++ tests/testthat/test-hilo.R
@@ -0,0 +1,28 @@
+test_that("hilo", {
+  # defaults
+  hl <- new_hilo()
+  expect_length(hl, 0)
+
+  # display
+  expect_output(print(hl), "<hilo\\[0\\]>")
+  expect_output(print(new_hilo(0,1,0.95)), "\\[0, 1\\]95")
+
+  dist <- dist_normal()
+
+  # hilo.distribution
+  hl <- hilo(dist, 0.95)
+  expect_length(hl, 1)
+  expect_equal(hl, new_hilo(qnorm(0.025), qnorm(0.975), 0.95))
+
+  # vec_math.hilo
+  expect_equal(is.na(hl), FALSE)
+  expect_equal(is.nan(hl), FALSE)
+
+  # vec_arith.hilo
+  expect_equal(
+    hl*3+1,
+    new_hilo(qnorm(0.025)*3+1, qnorm(0.975)*3+1, 0.95)
+  )
+  expect_equal(-hl, hl)
+  expect_equal(-3*hl, hl/(1/3))
+})
