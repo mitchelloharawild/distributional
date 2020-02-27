1b9d0262021f6dd055211a3a21a21ae1997874c3
Mitchell <mail@mitchelloharawild.com>
2020-02-27 22:05:46 +1100

    Fix hilo tests for interface change

--- tests/testthat/test-hilo.R
+++ tests/testthat/test-hilo.R
@@ -5,14 +5,14 @@ test_that("hilo", {
 
   # display
   expect_output(print(hl), "<hilo\\[0\\]>")
-  expect_output(print(new_hilo(0,1,0.95)), "\\[0, 1\\]95")
+  expect_output(print(new_hilo(0,1,95)), "\\[0, 1\\]95")
 
   dist <- dist_normal()
 
   # hilo.distribution
-  hl <- hilo(dist, 0.95)
+  hl <- hilo(dist, 95)
   expect_length(hl, 1)
-  expect_equal(hl, new_hilo(qnorm(0.025), qnorm(0.975), 0.95))
+  expect_equal(hl, new_hilo(qnorm(0.025), qnorm(0.975), 95))
 
   # vec_math.hilo
   expect_equal(is.na(hl), FALSE)
@@ -21,7 +21,7 @@ test_that("hilo", {
   # vec_arith.hilo
   expect_equal(
     hl*3+1,
-    new_hilo(qnorm(0.025)*3+1, qnorm(0.975)*3+1, 0.95)
+    new_hilo(qnorm(0.025)*3+1, qnorm(0.975)*3+1, 95)
   )
   expect_equal(-hl, hl)
   expect_equal(-3*hl, hl/(1/3))
