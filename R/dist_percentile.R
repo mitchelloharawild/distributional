ec965453aad8c1803f6980df0b6bed3a3270d3d1
Mitchell <mail@mitchelloharawild.com>
2020-04-07 16:52:20 +1000

    Added dist_percentile
    cc @robjhyndman

---
+++ R/dist_percentile.R
@@ -0,0 +1,57 @@
+#' Percentile distribution
+#'
+#' @param x A list of values
+#' @param percentile A list of percentiles
+#'
+#' @examples
+#' dist <- dist_normal()
+#' percentiles <- seq(0.01, 0.99, by = 0.01)
+#' x <- vapply(percentiles, quantile, double(1L), x = dist)
+#' dist_percentile(list(x), list(percentiles*100))
+#'
+#' @export
+dist_percentile <- function(x, percentile){
+  x <- as_list_of(x, .ptype = double())
+  percentile <- as_list_of(percentile, .ptype = double())
+  new_dist(x = x, percentile = percentile, class = "dist_percentile")
+}
+
+#' @export
+print.dist_percentile <- function(x, ...){
+  cat(format(x, ...))
+}
+
+#' @export
+format.dist_percentile <- function(x, ...){
+  sprintf(
+    "percentile[%s]",
+    length(x[["x"]])
+  )
+}
+
+# #' @export
+# density.dist_percentile <- function(x, at, ...){
+# }
+#
+
+#' @export
+quantile.dist_percentile <- function(x, p, ...){
+  approx(x = x[["percentile"]]/100, y = x[["x"]], xout = p)$y
+}
+
+#' @export
+cdf.dist_percentile <- function(x, q, ...){
+  approx(x = x[["x"]], y = x[["percentile"]]/100, xout = q)$y
+}
+#
+# #' @export
+# generate.dist_percentile <- function(x, times, ...){
+# }
+#
+# #' @export
+# mean.dist_percentile <- function(x, ...){
+# }
+#
+# #' @export
+# variance.dist_percentile <- function(x, ...){
+# }
