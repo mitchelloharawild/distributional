f86ff3827b46c2f0ac94d0ed578f4e2e3d316d7e
Mitchell <mail@mitchelloharawild.com>
2020-04-22 11:46:40 +1000

    Add early escape from bias adjustment if variance is unkown

--- R/transformed.R
+++ R/transformed.R
@@ -45,6 +45,7 @@ generate.dist_transformed <- function(x, ...){
 mean.dist_transformed <- function(x, ...){
   mu <- mean(x[["dist"]])
   sigma2 <- variance(x[["dist"]])
+  if(is.na(sigma2)) return(x[["transform"]](mu))
   x[["transform"]](mu) + numDeriv::hessian(x[["transform"]], mu)/2*sigma2
 }
 
