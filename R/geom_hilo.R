ecea4498b37205ca41baa4304a7679c32ffb6630
Mitchell <mail@mitchelloharawild.com>
2020-05-18 16:33:25 +1000

    Added lower bound for colour darkening

--- R/geom_hilo.R
+++ R/geom_hilo.R
@@ -88,7 +88,7 @@ GeomHiloRibbon <- ggplot2::ggproto(
 darken_fill <- function(col, prob) {
   col <- farver::convert_colour(t(grDevices::col2rgb(col)), "RGB", "HSL")
   n_prob <- length(unique(prob))
-  col[,3] <- seq(90 - (n_prob - 1)*10, 90, length.out = n_prob)[match(prob, sort(unique(prob)))]
+  col[,3] <- seq(90 - pmin((n_prob - 1)*10, 30), 90, length.out = n_prob)[match(prob, sort(unique(prob)))]
   col <- farver::convert_colour(col, "HSL", "RGB")
   col2hex(col)
 }
