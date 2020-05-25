0d3ed1a351353b9410ed174d4179c0aa10dc7ecf
Mitchell <mail@mitchelloharawild.com>
2020-05-25 23:24:13 +1000

    Added Poisson-Inverse Gaussian distribution

--- R/utils.R
+++ R/utils.R
@@ -39,3 +39,11 @@ is_latex_output <- function() {
   }
   get("is_latex_output", asNamespace("knitr"))()
 }
+
+require_package <- function(pkg){
+  if(!requireNamespace(pkg, quietly = TRUE)){
+    abort(
+      sprintf('The `%s` package must be installed to use this functionality. It can be installed with install.packages("%s")', pkg, pkg)
+    )
+  }
+}
