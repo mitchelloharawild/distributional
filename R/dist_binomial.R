dc6b3afa1dd3a2d9c300d6bf0bec7bb9a682992a
Mitchell <mail@mitchelloharawild.com>
2020-05-25 16:08:38 +1000

    Added Binomial distribution

---
+++ R/dist_binomial.R
@@ -0,0 +1,71 @@
+#' The Binomial distribution
+#'
+#' @param size The number of trials.
+#' @param prob The probability of success on each trial.
+#'
+#' @examples
+#' dist_binomial(size = 1:5, prob = c(0.05, 0.5, 0.3, 0.9, 0.1))
+#'
+#' @name dist_bimonial
+#' @export
+dist_binomial <- function(size, prob){
+  size <- vec_cast(size, integer())
+  prob <- vec_cast(prob, double())
+  if(any(size < 0)){
+    abort("The number of observations cannot be negative.")
+  }
+  if(any((prob < 0) | (prob > 1))){
+    abort("The probability of success must be between 0 and 1.")
+  }
+  new_dist(n = size, p = prob, class = "dist_binomial")
+}
+
+#' @export
+print.dist_binomial <- function(x, ...){
+  cat(format(x, ...))
+}
+
+#' @export
+format.dist_binomial <- function(x, digits = 2, ...){
+  sprintf(
+    "B(%s, %s)",
+    format(x[["n"]], digits = digits, ...),
+    format(x[["p"]]^2, digits = digits, ...)
+  )
+}
+
+#' @rdname dist_bimonial
+#' @export
+density.dist_binomial <- function(x, at, ...){
+  stats::dbinom(at, x[["n"]], x[["p"]])
+}
+
+#' @rdname dist_bimonial
+#' @export
+quantile.dist_binomial <- function(x, p, ...){
+  stats::qbinom(p, x[["n"]], x[["p"]])
+}
+
+#' @rdname dist_bimonial
+#' @export
+cdf.dist_binomial <- function(x, q, ...){
+  stats::pbinom(q, x[["n"]], x[["p"]])
+}
+
+#' @rdname dist_bimonial
+#' @export
+generate.dist_binomial <- function(x, times, ...){
+  stats::rbinom(times, x[["n"]], x[["p"]])
+}
+
+#' @rdname dist_bimonial
+#' @export
+mean.dist_binomial <- function(x, ...){
+  x[["n"]]*x[["p"]]
+}
+
+#' @rdname dist_bimonial
+#' @export
+variance.dist_binomial <- function(x, ...){
+  x[["n"]]*x[["p"]]*(1-x[["p"]])
+}
