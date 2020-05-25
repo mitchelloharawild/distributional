b6cd258a5a431b63d5e719d81b72ed0e255ad845
Mitchell <mail@mitchelloharawild.com>
2020-05-25 21:58:29 +1000

    Added Studentized range distribution

---
+++ R/dist_studentized_range.R
@@ -0,0 +1,42 @@
+#' The Studentized Range distribution
+#'
+#' \lifecycle{stable}
+#'
+#' @inheritParams stats::qtukey
+#'
+#' @seealso qtukey
+#'
+#' @examples
+#' dist_studentized_range(nmeans = c(6, 2), df = c(5, 4), nranges = c(1, 1))
+#'
+#' @name dist_studentized_range
+#' @export
+dist_studentized_range <- function(nmeans, df, nranges){
+  nmeans <- vec_cast(nmeans, double())
+  df <- vec_cast(df, double())
+  new_dist(nm = nmeans, df = df, nr = nranges, class = "dist_studentized_range")
+}
+
+#' @export
+print.dist_studentized_range <- function(x, ...){
+  cat(format(x, ...))
+}
+
+#' @export
+format.dist_studentized_range <- function(x, digits = 2, ...){
+  sprintf(
+    "StudentizedRange(%s, %s)",
+    format(x[["nm"]], digits = digits, ...),
+    format(x[["df"]], digits = digits, ...)
+  )
+}
+
+#' @export
+quantile.dist_studentized_range <- function(x, p, ...){
+  stats::qtukey(p, x[["nm"]], x[["df"]], x[["nr"]])
+}
+
+#' @export
+cdf.dist_studentized_range <- function(x, q, ...){
+  stats::ptukey(q, x[["nm"]], x[["df"]], x[["nr"]])
+}
