a093aeef194df28027fc11339117c2406ce061c7
Mitchell <mail@mitchelloharawild.com>
2020-05-25 16:23:47 +1000

    Exported inflated distributions

--- R/inflated.R
+++ R/inflated.R
@@ -1,12 +1,20 @@
-dist_inflated <- function(dist, p, x = 0){
+#' Inflate a value of a probability distribution
+#'
+#' @param dist The distribution(s) to inflate.
+#' @param prob The added probability of observing `x`.
+#' @param x The value to inflate. The default of `x = 0` is for zero-inflation.
+#'
+#' @name dist_inflated
+#' @export
+dist_inflated <- function(dist, prob, x = 0){
   vec_is(dist, new_dist())
   vec_is(x, numeric())
   vec_is(x, numeric())
-  if(p < 0 || p > 1){
+  if(prob < 0 || prob > 1){
     abort("The inflation probability must be between 0 and 1.")
   }
-  infl <- dist_scaled(dist_degenerate(x), p)
-  dist <- dist_scaled(dist, 1-p)
+  infl <- dist_scaled(dist_degenerate(x), prob)
+  dist <- dist_scaled(dist, 1-prob)
   new_dist(dist, infl,
            dimnames = dimnames(dist), class = c("dist_inflated", "dist_mixture"))
 }
