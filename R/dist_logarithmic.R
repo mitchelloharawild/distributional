911b54818894b4b27d315c49810ff41a92e52bd7
Mitchell <mail@mitchelloharawild.com>
2020-05-26 13:31:46 +1000

    Added Logarithmic distribution

---
+++ R/dist_logarithmic.R
@@ -0,0 +1,69 @@
+#' The Logarithmic distribution
+#'
+#' \lifecycle{stable}
+#'
+#' @inheritParams actuar::dlogarithmic
+#'
+#' @seealso actuar::dlogarithmic
+#'
+#' @examples
+#' dist_logarithmic(prob = c(0.33, 0.66, 0.99))
+#'
+#' @name dist_logarithmic
+#' @export
+dist_logarithmic <- function(prob){
+  prob <- vec_cast(prob, double())
+  if(any((prob < 0) | (prob > 1))){
+    abort("The prob parameter of a Logarithmic distribution must be between 0 and 1.")
+  }
+  new_dist(p = prob, class = "dist_logarithmic")
+}
+
+#' @export
+print.dist_logarithmic <- function(x, ...){
+  cat(format(x, ...))
+}
+
+#' @export
+format.dist_logarithmic <- function(x, digits = 2, ...){
+  sprintf(
+    "logarithmic(%s)",
+    format(x[["p"]], digits = digits, ...)
+  )
+}
+
+#' @export
+density.dist_logarithmic <- function(x, at, ...){
+  require_package("actuar")
+  actuar::dlogarithmic(at, x[["p"]])
+}
+
+#' @export
+quantile.dist_logarithmic <- function(x, p, ...){
+  require_package("actuar")
+  actuar::qlogarithmic(p, x[["p"]])
+}
+
+#' @export
+cdf.dist_logarithmic <- function(x, q, ...){
+  require_package("actuar")
+  actuar::plogarithmic(q, x[["p"]])
+}
+
+#' @export
+generate.dist_logarithmic <- function(x, times, ...){
+  require_package("actuar")
+  actuar::rlogarithmic(times, x[["p"]])
+}
+
+#' @export
+mean.dist_logarithmic <- function(x, ...){
+  p <- x[["p"]]
+  (-1/(log(1-p)))*(p/(1-p))
+}
+
+#' @export
+variance.dist_logarithmic <- function(x, ...){
+  p <- x[["p"]]
+  -(p^2 + p*log(1-p))/((1-p)*log(1-p))^2
+}
