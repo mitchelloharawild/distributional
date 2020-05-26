799d7b99d592073a56f8d374ca4d808b2479d16d
Mitchell <mail@mitchelloharawild.com>
2020-05-26 11:18:05 +1000

    Added Inverse Exponential distribution

---
+++ R/dist_inverse_exponential.R
@@ -0,0 +1,67 @@
+#' The Inverse Exponential distribution
+#'
+#' \lifecycle{stable}
+#'
+#' @inheritParams actuar::dinvexp
+#'
+#' @seealso actuar::dinvexp
+#'
+#' @examples
+#' dist_inverse_exponential(rate = 1:5)
+#'
+#' @name dist_inverse_exponential
+#' @export
+dist_inverse_exponential <- function(rate){
+  rate <- vec_cast(rate, double())
+  if(any(rate <= 0)){
+    abort("The rate parameter of a Inverse Exponential distribution must be strictly positive.")
+  }
+  new_dist(r = rate, class = "dist_inverse_exponential")
+}
+
+#' @export
+print.dist_inverse_exponential <- function(x, ...){
+  cat(format(x, ...))
+}
+
+#' @export
+format.dist_inverse_exponential <- function(x, digits = 2, ...){
+  sprintf(
+    "InvExp(%s)",
+    format(x[["r"]], digits = digits, ...)
+  )
+}
+
+#' @export
+density.dist_inverse_exponential <- function(x, at, ...){
+  require_package("actuar")
+  actuar::dinvexp(at, x[["r"]])
+}
+
+#' @export
+quantile.dist_inverse_exponential <- function(x, p, ...){
+  require_package("actuar")
+  actuar::qinvexp(p, x[["r"]])
+}
+
+#' @export
+cdf.dist_inverse_exponential <- function(x, q, ...){
+  require_package("actuar")
+  actuar::pinvexp(q, x[["r"]])
+}
+
+#' @export
+generate.dist_inverse_exponential <- function(x, times, ...){
+  require_package("actuar")
+  actuar::rinvexp(times, x[["r"]])
+}
+
+#' @export
+mean.dist_inverse_exponential <- function(x, ...){
+  NA_real_
+}
+
+#' @export
+variance.dist_inverse_exponential <- function(x, ...){
+  NA_real_
+}
