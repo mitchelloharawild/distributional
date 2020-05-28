9e5450b9efaa1400b4039e0d71aa2150c3ddd3dc
Mitchell <mail@mitchelloharawild.com>
2020-05-28 17:30:10 +1000

    Updated dist_mixture to be self contained with its own methods

--- R/mixture.R
+++ R/mixture.R
@@ -5,7 +5,9 @@
 #' @param ... Distributions to be used in the mixture.
 #' @param weights The weight of each distribution passed to `...`.
 #'
-#' @name dist_mixture
+#' @examples
+#' dist_mixture(dist_normal(0, 1), dist_normal(5, 2), weights = c(0.3, 0.7))
+#'
 #' @export
 dist_mixture <- function(..., weights = numeric()){
   dist <- dots_list(...)
@@ -16,8 +18,7 @@ dist_mixture <- function(..., weights = numeric()){
   if(any(weights < 0)){
     abort("All weights in a mixtue model must be non-negative.")
   }
-  dist <- mapply(dist_scaled, dist = dist, scale = weights, SIMPLIFY = FALSE)
-  new_dist(!!!dist, class = "dist_mixture")
+  new_dist(dist = transpose(dist), w = list(weights), class = "dist_mixture")
 }
 
 #' @export
@@ -30,10 +31,47 @@ format.dist_mixture <- function(x, ...){
 
 #' @export
 density.dist_mixture <- function(x, ...){
-  sum(vapply(x, density, numeric(1L), ...))
+  sum(x[["w"]]*vapply(x[["dist"]], density, numeric(1L), ...))
+}
+
+#' @export
+quantile.dist_mixture <- function(x, p, ...){
+  # Find bounds for optimisation based on range of each quantile
+  dist_q <- vapply(x[["dist"]], quantile, numeric(1L), p, ...)
+
+  # Search the cdf() for appropriate quantile
+  stats::optimise(
+    function(pos) (p - cdf(x, pos, ...))^2,
+    interval = c(min(dist_q), max(dist_q))
+  )$minimum
 }
 
 #' @export
 cdf.dist_mixture <- function(x, ...){
-  sum(vapply(x, cdf, numeric(1L), ...))
+  sum(x[["w"]]*vapply(x[["dist"]], cdf, numeric(1L), ...))
+}
+
+#' @export
+generate.dist_mixture <- function(x, times, ...){
+  dist_idx <- .bincode(stats::runif(times), breaks = c(0, cumsum(x[["w"]])))
+  r <- numeric(times)
+  for(i in seq_along(x[["dist"]])){
+    r_pos <- dist_idx == i
+    r[r_pos] <- generate(x[["dist"]][[i]], sum(r_pos), ...)
+  }
+  r
+}
+
+#' @export
+mean.dist_mixture <- function(x, ...){
+  sum(x[["w"]]*vapply(x[["dist"]], mean, numeric(1L), ...))
+}
+
+#' @export
+variance.dist_mixture <- function(x, ...){
+  m <- vapply(x[["dist"]], mean, numeric(1L), ...)
+  v <- vapply(x[["dist"]], variance, numeric(1L), ...)
+  m1 <- sum(x[["w"]]*m)
+  m2 <- sum(x[["w"]]*(m^2 + v))
+  m2 - m1^2
 }
