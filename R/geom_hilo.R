3ba9bba12bb67468fc580f1887156b1c90c87bc3
Mitchell <mail@mitchelloharawild.com>
2020-05-29 01:37:58 +1000

    Added geom_hilo_linerange

--- R/geom_hilo.R
+++ R/geom_hilo.R
@@ -1,5 +1,7 @@
 #' Ribbon plots for hilo intervals
 #'
+#' \lifecycle{maturing}
+#'
 #' `geom_hilo_ribbon()` displays the interval defined by a hilo object. The
 #' luminance of the shaded area indicates its confidence level. The shade colour
 #' can be controlled by the `fill` aesthetic, however the luminance will be
@@ -7,8 +9,8 @@
 #'
 #' @inheritParams ggplot2::geom_ribbon
 #'
-# #' @seealso
-# #'   [`geom_hilo_linerange()`] for discrete hilo intervals (lines)
+#' @seealso
+#'   [`geom_hilo_linerange()`] for discrete hilo intervals (vertical lines)
 #'
 #' @examples
 #' dist <- dist_normal(1:3, 1:3)
@@ -74,6 +76,87 @@ GeomHiloRibbon <- ggplot2::ggproto(
       panel_scales, coord
     )
     ggplot2:::ggname("geom_hilo_ribbon", do.call(grid::grobTree, grobs))
+  }
+)
+
+#' Line ranges for hilo intervals
+#'
+#' \lifecycle{experimental}
+#'
+#' `geom_hilo_linerange()` displays the interval defined by a hilo object. The
+#' luminance of the shaded area indicates its confidence level. The shade colour
+#' can be controlled by the `fill` aesthetic, however the luminance will be
+#' overwritten to represent the confidence level.
+#'
+#' @inheritParams ggplot2::geom_linerange
+#'
+#' @seealso
+#'   [`geom_hilo_ribbon()`] for continuous hilo intervals (ribbons)
+#'
+#' @examples
+#' dist <- dist_normal(1:3, 1:3)
+#' library(ggplot2)
+#' ggplot(
+#'   data.frame(x = rep(1:3, 2), interval = c(hilo(dist, 80), hilo(dist, 95)))
+#' ) +
+#'   geom_hilo_linerange(aes(x = x, hilo = interval))
+#'
+#' @export
+geom_hilo_linerange <- function(mapping = NULL, data = NULL, stat = "identity",
+                             position = "identity", na.rm = FALSE,
+                             show.legend = NA, inherit.aes = TRUE, ...) {
+  if(!is.null(mapping$hilo)){
+    mapping$ymin <- expr(ggplot2::after_stat(vctrs::vec_data(hilo)$lower))
+    mapping$ymax <- expr(ggplot2::after_stat(vctrs::vec_data(hilo)$upper))
+    mapping$level <- expr(ggplot2::after_stat(vctrs::vec_data(hilo)$level))
+  }
+  ggplot2::layer(
+    geom = GeomHiloLinerange, mapping = mapping, data = data, stat = stat,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+GeomHiloLinerange <- ggplot2::ggproto(
+  "GeomHiloLinerange", ggplot2::GeomLinerange,
+  required_aes = c(ggplot2::GeomLinerange$required_aes),
+  optional_aes = c("hilo", "level"),
+  default_aes = ggplot2::aes(
+    colour = "grey20", fill = "grey60", size = 2,
+    linetype = 1, weight = 1, alpha = 1, level = NA
+  ),
+  draw_key = function(data, params, size) {
+    lwd <- min(data$size, min(size) / 4)
+    # Calculate and set colour
+    fillcol <- darken_fill(data$fill, 80)
+
+    grid::rectGrob(
+      width = grid::unit(1, "npc") - grid::unit(lwd, "mm"),
+      height = grid::unit(1, "npc") - grid::unit(lwd, "mm"),
+      gp = grid::gpar(
+        col = fillcol,
+        fill = scales::alpha(fillcol, data$alpha),
+        lty = data$linetype,
+        lwd = lwd * ggplot2::.pt,
+        linejoin = "mitre"
+      )
+    )
+  },
+
+  draw_panel = function(data, panel_scales, coord) {
+    # Calculate colour
+    data$colour <- darken_fill(data$colour, data$level)
+    data$fill <- NA
+    # Compute alpha transparency
+    data$alpha <- grDevices::col2rgb(data$colour, alpha = TRUE)[4, ] / 255 * data$alpha
+
+    # Create grobs
+    grobs <- lapply(
+      split(data, -data$level),
+      ggplot2::GeomLinerange$draw_panel,
+      panel_scales, coord
+    )
+    ggplot2:::ggname("geom_hilo_linerange", do.call(grid::grobTree, grobs))
   }
 )
 
--- tests/testthat/test-graphics.R
+++ tests/testthat/test-graphics.R
@@ -56,12 +56,28 @@ test_that("geom_hilo_ribbon()", {
     geom_hilo_ribbon(ggplot2::aes(x = x, hilo = interval))
   expect_silent(print(p))
   expect_length(
-    ggplot2::layer_data(p)$y,
+    ggplot2::layer_data(p)$hilo,
     3*2
   )
   expect_equal(
     ggplot2::layer_data(p)$hilo,
     c(hilo(dist, 80), hilo(dist, 95))[c(1,4,2,5,3,6)]
+  )
+})
+test_that("geom_hilo_linerange()", {
+  dist <- dist_normal(1:3, 1:3)
+  p <- ggplot2::ggplot(
+    data.frame(x = rep(1:3, 2), interval = c(hilo(dist, 80), hilo(dist, 95)))
+  ) +
+    geom_hilo_linerange(ggplot2::aes(x = x, hilo = interval))
+  expect_silent(print(p))
+  expect_length(
+    ggplot2::layer_data(p)$hilo,
+    3*2
+  )
+  expect_equal(
+    ggplot2::layer_data(p)$hilo,
+    c(hilo(dist, 80), hilo(dist, 95))
   )
 })
 
