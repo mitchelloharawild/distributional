63110333d18e5c372de097ecf5df88cddb35110b
Matthew Kay <matthew.kay@gmail.com>
2020-07-11 22:48:59 -0400

    convert t distribution to location-scale form

--- R/dist_student_t.R
+++ R/dist_student_t.R
@@ -1,13 +1,16 @@
-#' The (non-central) Student t Distribution
+#' The (non-central) location-scale Student t Distribution
 #'
 #' \lifecycle{stable}
 #'
 #' @inheritParams stats::dt
+#' @param mu The location parameter of the distribution.
+#'   If `ncp == 0` (or `NULL`), this is the median.
+#' @param sigma The scale parameter of the distribution.
 #'
 #' @seealso [stats::TDist]
 #'
 #' @examples
-#' dist_student_t(df = c(1,2,5))
+#' dist_student_t(df = c(1,2,5), mu = c(0,1,2), sigma = c(1,2,3))
 #'
 #' @name dist_student_t
 #' @export
@@ -43,29 +46,30 @@ format.dist_student_t <- function(x, digits = 2, ...){
 #' @export
 density.dist_student_t <- function(x, at, ...){
   ncp <- x[["ncp"]] %||% missing_arg()
+  sigma <- x[["sigma"]]
 
-  stats::dt(at, x[["df"]], ncp)
+  stats::dt((at - x[["mu"]])/sigma, x[["df"]], ncp) / sigma
 }
 
 #' @export
 quantile.dist_student_t <- function(x, p, ...){
   ncp <- x[["ncp"]] %||% missing_arg()
 
-  stats::qt(p, x[["df"]], ncp)
+  stats::qt(p, x[["df"]], ncp) * x[["sigma"]] + x[["mu"]]
 }
 
 #' @export
 cdf.dist_student_t <- function(x, q, ...){
   ncp <- x[["ncp"]] %||% missing_arg()
 
-  stats::pt(q, x[["df"]], ncp)
+  stats::pt((q - x[["mu"]])/x[["sigma"]], x[["df"]], ncp)
 }
 
 #' @export
 generate.dist_student_t <- function(x, times, ...){
   ncp <- x[["ncp"]] %||% missing_arg()
 
-  stats::rt(times, x[["df"]], ncp)
+  stats::rt(times, x[["df"]], ncp) * x[["sigma"]] + x[["mu"]]
 }
 
 #' @export
@@ -73,20 +77,21 @@ mean.dist_student_t <- function(x, ...){
   df <- x[["df"]]
   if(df <= 1) return(NA_real_)
   if(is.null(x[["ncp"]])){
-    0
+    x[["mu"]]
   } else {
-    x[["ncp"]] * sqrt(df/2) * (gamma((df-1)/2)/gamma(df/2))
+    x[["mu"]] + x[["ncp"]] * sqrt(df/2) * (gamma((df-1)/2)/gamma(df/2)) * x[["sigma"]]
   }
 }
 
 #' @export
 variance.dist_student_t <- function(x, ...){
   df <- x[["df"]]
+  ncp <- x[["ncp"]]
   if(df <= 1) return(NA_real_)
   if(df <= 2) return(Inf)
-  if(is.null(x[["ncp"]])){
-    df / (df - 2)
+  if(is.null(ncp)){
+    df / (df - 2) * x[["sigma"]]^2
   } else {
-    (df*(1+x[["ncp"]]^2))/(df-2) - mean(x)^2
+    ((df*(1+ncp^2))/(df-2) - (ncp * sqrt(df/2) * (gamma((df-1)/2)/gamma(df/2)))^2) * x[["sigma"]]^2
   }
 }
