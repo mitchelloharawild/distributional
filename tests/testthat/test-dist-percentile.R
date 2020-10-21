451fb89ed44134746f5e7aa6f7fe53855ebc92c4
David Holt <david@iMac-2015.local>
2020-10-21 13:26:55 -0400

    test quantile with multiple probs

--- tests/testthat/test-dist-percentile.R
+++ tests/testthat/test-dist-percentile.R
@@ -3,12 +3,17 @@ test_that("Negative Binomial distribution", {
   percentiles <- seq(0.01, 0.99, by = 0.01)
   x <- vapply(percentiles, quantile, double(1L), x = dist)
   dist <- dist_percentile(list(x), list(percentiles*100))
+  dists <- c(dist_normal(0, 1), dist_normal(1, 2))
 
   expect_equal(format(dist), "percentile[99]")
 
   # quantiles
   expect_equal(quantile(dist, 0.6), stats::qnorm(0.6, 0, 1))
   expect_equal(quantile(dist, 0.61), stats::qnorm(0.61, 0, 1))
+  expect_equal(quantile(dist, p = c(.6, .61)), c(stats::qnorm(0.6, 0, 1), stats::qnorm(0.61, 0, 1)))
+  expect_equal(quantile(dists, p = .6), c(stats::qnorm(0.6, 0, 1), stats::qnorm(0.6, 1, 2)))
+  expect_error(quantile(dists, p = c(.6, .61)))
+
 
   # pdf
 
