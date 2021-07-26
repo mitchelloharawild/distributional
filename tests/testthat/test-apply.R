21a70dd6177dfcc4de0fc4868e897704fafea27e
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-07-26 14:34:56 +1000

    Added unit tests for applying inputs over multivariate distributions

--- tests/testthat/test-apply.R
+++ tests/testthat/test-apply.R
@@ -46,3 +46,51 @@ test_that("Recycling rules and output for applying multiple inputs over multiple
     new_data_frame(list(a = quantile(dist, ppoints(10)), b = quantile(dist, 0.5)))
   )
 })
+
+test_that("Recycling rules and output for applying multiple inputs over multiple multivariate distributions", {
+  # <dist> is a bivariate distribution vector of length 2
+  dist <- dist_multivariate_normal(mu = list(c(1,2), c(3,5)),
+                                   sigma = list(matrix(c(4,2,2,3), ncol=2),
+                                                matrix(c(5,1,1,4), ncol=2)))
+  dimnames(dist) <- c("a", "b")
+
+  expect_equal(
+    quantile(dist, 0.5),
+    matrix(c(1,3,2,5), nrow = 2, dimnames = list(NULL, c("a", "b")))
+  )
+
+  expect_equal(
+    quantile(dist, c(0.5, 0.9)),
+    matrix(c(1, 5.86563641722901, 2, 7.5631031310892), nrow = 2, dimnames = list(NULL, c("a", "b")))
+  )
+
+  expect_error(
+    quantile(dist, c(0.5, 0.9, 0.95)),
+    "Cannot recycle input"
+  )
+
+  expect_equal(
+    quantile(dist, list(single = 0.5, varied = c(0.8, 0.3))),
+    new_data_frame(
+      list(single = quantile(dist, 0.5), varied = quantile(dist, c(0.8, 0.3)))
+    )
+  )
+
+  expect_equal(
+    density(dist, cbind(2, 3)),
+    c(0.046649277604197, 0.0215708514518913)
+  )
+
+  expect_equal(
+    mean(dist),
+    matrix(c(1,3,2,5), nrow = 2, dimnames = list(NULL, c("a", "b")))
+  )
+
+  expect_equal(
+    variance(dist),
+    list(
+      matrix(c(4,2,2,3), nrow = 2),
+      matrix(c(5,1,1,4), nrow = 2)
+    )
+  )
+})
