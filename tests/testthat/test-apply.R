80d38ad56105872bf621858b9d3ae0d8d7a586d6
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-07-26 14:28:40 +1000

    Added unit tests for applying inputs over many univariate distributions

---
+++ tests/testthat/test-apply.R
@@ -0,0 +1,48 @@
+test_that("Recycling rules and output for applying multiple inputs over multiple univariate distributions", {
+  # <dist> is a distribution vector of length 10
+  dist <- dist_normal(1:10, 1:10)
+
+  # p = 0.5:          apply p across all elements of <dist> (recycling p onto <dist>)
+  #                   Returns a vector of length 10
+  expect_equal(
+    quantile(dist, 0.5),
+    qnorm(0.5, 1:10, 1:10)
+  )
+
+  # p = c(0.5, 0.9):  Cannot recycle p (length 2) onto <dist> (length 10)
+  #                   Returns an error.
+  expect_error(
+    quantile(dist, c(0.5, 0.9)),
+    "Cannot recycle input"
+  )
+
+  # p = ppoints(10):  apply each p to each element of <dist> (no recycling)
+  #                   Returns a vector of length 10
+  expect_equal(
+    quantile(dist, ppoints(10)),
+    qnorm(ppoints(10), 1:10, 1:10)
+  )
+
+  # p = list(0.5):    equivalent to p = 0.5, but returns a list output
+  #                   Returns a tibble with 1 vector column of length 10
+  expect_equal(
+    quantile(dist, list(a = 0.5)),
+    new_data_frame(list(a = quantile(dist, 0.5)))
+  )
+
+  # p = list(c(0.5, 0.9)):
+  #                   Cannot recycle p[[1]] (length 2) onto <dist> (length 10)
+  #                   Returns an error.
+  expect_error(
+    quantile(dist, list(a = c(0.5, 0.9))),
+    "Cannot recycle input"
+  )
+
+  # p = list(p1, 0.5): equivalent to df(quantile(<dist>, p1), quantile(<dist>, 0.5)).
+  #                   Returns a tibble with 2 vector columns of length 10
+  #                   Names of p are used in output.
+  expect_equal(
+    quantile(dist, list(a=ppoints(10), b=0.5)),
+    new_data_frame(list(a = quantile(dist, ppoints(10)), b = quantile(dist, 0.5)))
+  )
+})
