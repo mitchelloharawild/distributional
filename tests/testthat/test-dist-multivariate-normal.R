29ee8c7d72850192952d3ff1262b014154e2ebcb
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-10-04 15:53:17 +1100

    Avoid hard coded multivariate normal test values

--- tests/testthat/test-dist-multivariate-normal.R
+++ tests/testthat/test-dist-multivariate-normal.R
@@ -21,15 +21,15 @@ test_that("Multivariate normal distribution", {
   )
 
   skip_if_not_installed("mvtnorm")
-  expect_equal(quantile(dist, 0.1, type = "equicoordinate"), 0.131134182424683) # dput(mvtnorm::qmvnorm(0.1, mean = mu, sigma = sigma))
+  expect_equivalent(quantile(dist, 0.1, type = "equicoordinate"), mvtnorm::qmvnorm(0.1, mean = mu, sigma = sigma)$quantile)
 
   # pdf
-  expect_equal(density(dist, cbind(1, 2)), 0.0562697697598191) # dput(mvtnorm::dmvnorm(c(1, 2), mean = mu, sigma = sigma))
-  expect_equal(density(dist, cbind(-3, 4)), 0.000139478814272667) # dput(mvtnorm::dmvnorm(c(-3, 4), mean = mu, sigma = sigma))
+  expect_equal(density(dist, cbind(1, 2)), mvtnorm::dmvnorm(c(1, 2), mean = mu, sigma = sigma))
+  expect_equal(density(dist, cbind(-3, 4)), mvtnorm::dmvnorm(c(-3, 4), mean = mu, sigma = sigma))
 
   # cdf
-  expect_equal(cdf(dist, cbind(1, 2)), 0.347956638007652) # dput(mvtnorm::pmvnorm(upper = c(1,2), mean = mu, sigma = sigma))
-  expect_equal(cdf(dist, cbind(-3, 4)), 0.124078569373413) # dput(mvtnorm::pmvnorm(c(-3, 4), mean = mu, sigma = sigma))
+  expect_equivalent(cdf(dist, cbind(1, 2)), mvtnorm::pmvnorm(upper = c(1,2), mean = mu, sigma = sigma))
+  expect_equivalent(cdf(dist, cbind(-3, 4)), mvtnorm::pmvnorm(c(-3, 4), mean = mu, sigma = sigma))
 
   # F(Finv(a)) ~= a
   # expect_equal(cdf(dist, list(as.numeric(quantile(dist, 0.53)))), 0.53, tolerance = 1e-3)
