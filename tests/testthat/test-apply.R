9b50cbb89e6eedb7795ef3dcc311d207f78c8a4b
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-10-29 15:08:38 +1100

    Update tests for dist_apply changes

--- tests/testthat/test-apply.R
+++ tests/testthat/test-apply.R
@@ -10,17 +10,19 @@ test_that("Recycling rules and output for applying multiple inputs over multiple
   )
 
   # p = c(0.5, 0.9):  Cannot recycle p (length 2) onto <dist> (length 10)
-  #                   Returns an error.
-  expect_error(
+  #                   Returns a list containing values for each p.
+  expect_equal(
     quantile(dist, c(0.5, 0.9)),
-    "Cannot recycle input"
+    mapply({function(mean, sd) qnorm(c(0.5, 0.9), mean, sd)},
+           mean = 1:10, sd = 1:10, SIMPLIFY = FALSE)
   )
 
   # p = ppoints(10):  apply each p to each element of <dist> (no recycling)
-  #                   Returns a vector of length 10
+  #                   Returns a list for each distribution with the 10 quantiles.
   expect_equal(
     quantile(dist, ppoints(10)),
-    qnorm(ppoints(10), 1:10, 1:10)
+    mapply({function(mean, sd) qnorm(ppoints(10), mean, sd)},
+           mean = 1:10, sd = 1:10, SIMPLIFY = FALSE)
   )
 
   # p = list(0.5):    equivalent to p = 0.5, but returns a list output
@@ -43,7 +45,7 @@ test_that("Recycling rules and output for applying multiple inputs over multiple
   #                   Names of p are used in output.
   expect_equal(
     quantile(dist, list(a=ppoints(10), b=0.5)),
-    new_data_frame(list(a = quantile(dist, ppoints(10)), b = quantile(dist, 0.5)))
+    new_data_frame(list(a = qnorm(ppoints(10), 1:10, 1:10), b = quantile(dist, 0.5)))
   )
 })
 
@@ -61,18 +63,24 @@ test_that("Recycling rules and output for applying multiple inputs over multiple
 
   expect_equal(
     quantile(dist, c(0.5, 0.9)),
-    matrix(c(1, 5.86563641722901, 2, 7.5631031310892), nrow = 2, dimnames = list(NULL, c("a", "b")))
+    list(matrix(c(1, 3.5631031310892, 2, 4.21971242404268), ncol = 2, dimnames = list(NULL, c("a", "b"))),
+         matrix(c(3, 5.86563641722901, 5, 7.5631031310892), ncol = 2, dimnames = list(NULL, c("a", "b"))))
   )
 
-  expect_error(
+  expect_equal(
     quantile(dist, c(0.5, 0.9, 0.95)),
-    "Cannot recycle input"
+    list(
+      matrix(c(1, 3.5631031310892, 4.28970725390294, 2, 4.21971242404268, 4.84897005289389),
+             ncol = 2, dimnames = list(NULL, c("a","b"))),
+      matrix(c(3, 5.86563641722901, 6.67800452290057, 5, 7.5631031310892, 8.28970725390294),
+             ncol = 2, dimnames = list(NULL, c("a", "b")))
+    )
   )
 
   expect_equal(
     quantile(dist, list(single = 0.5, varied = c(0.8, 0.3))),
     new_data_frame(
-      list(single = quantile(dist, 0.5), varied = quantile(dist, c(0.8, 0.3)))
+      list(single = quantile(dist, 0.5), varied = rbind(quantile(dist[1], 0.8), quantile(dist[2], 0.3)))
     )
   )
 
@@ -87,7 +95,7 @@ test_that("Recycling rules and output for applying multiple inputs over multiple
   )
 
   expect_equal(
-    variance(dist),
+    covariance(dist),
     list(
       matrix(c(4,2,2,3), nrow = 2),
       matrix(c(5,1,1,4), nrow = 2)
