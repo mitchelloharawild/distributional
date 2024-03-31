bff99b613e49f8e9e0f101f203a68198686971eb
Ven Popov <vencislav.popov@gmail.com>
2024-03-31 12:42:57 +0200

    add interval type (open, closed) to suppport region

--- R/support.R
+++ R/support.R
@@ -3,8 +3,8 @@
 #' @param x A list of prototype vectors defining the distribution type.
 #' @param limits A list of value limits for the distribution.
 #'
-new_support_region <- function(x, limits = NULL) {
-  vctrs::new_rcrd(list(x = x, lim = limits), class = "support_region")
+new_support_region <- function(x, limits = NULL, interval = list(c('closed','closed'))) {
+  vctrs::new_rcrd(list(x = x, lim = limits, interval = interval), class = "support_region")
 }
 
 #' @export
@@ -21,15 +21,17 @@ format.support_region <- function(x, ...) {
     }
     out
   }, FUN.VALUE = character(1L))
-  mapply(function(type, z) {
+  mapply(function(type, z, i) {
+    br1 <- switch(i[1], open = "(", closed = "[")
+    br2 <- switch(i[2], open = ")", closed = "]")
     if(any(is.na(z)) || all(is.infinite(z))) type
     else if (type == "Z" && identical(z[2], Inf)) {
-      if(z[1] == 0L) "N0" else if (z[2] == 1L) "N+" else paste0("[", z[1], ",", z[1]+1L, ",...,", z[2], "]")
+      if(z[1] == 0L) "N0" else if (z[2] == 1L) "N+" else paste0(br1, z[1], ",", z[1]+1L, ",...,", z[2], br2)
     }
-    else if (type == "R") paste0("[", z[1], ",", z[2], "]")
-    else if (type == "Z") paste0("[", z[1], ",", z[1]+1L, ",...,", z[2], "]")
+    else if (type == "R") paste0(br1, z[1], ",", z[2], br2)
+    else if (type == "Z") paste0(br1, z[1], ",", z[1]+1L, ",...,", z[2], br2)
     else type
-  }, type, field(x, "lim"))
+  }, type, field(x, "lim"), field(x, "interval"))
 }
 
 #' @export
