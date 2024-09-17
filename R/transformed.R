9e632ccf9344671c227325eeb8161ba8abe92fc9
mitchelloharawild <mitch.ohara-wild@monash.edu>
2024-09-17 11:00:30 +1000

    Skip monotonic transformation check if function is identity

--- R/transformed.R
+++ R/transformed.R
@@ -159,6 +159,9 @@ Ops.dist_transformed <- function(e1, e2) {
 }
 
 monotonic_increasing <- function(f, support) {
+  # Shortcut for identity function (used widely in ggdist)
+  if(identical(body(f), as.name(names(formals(f))))) return(TRUE)
+
   # Currently assumes (without checking, #9) monotonicity of f over the domain
   x <- f(field(support, "lim")[[1]])
   x[[2L]] > x[[1L]]
